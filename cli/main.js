1163414f8b08efca7cb50bc395051b85bc3abd87
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-06-05 20:31:55 +0900

    use semantic

--- cli/main.js
+++ cli/main.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import SuiVue from 'semantic-ui-vue'
 
 import App from './App'
 import router from './router.js'
 import store from './store'
+
+import 'semantic-ui-css/semantic.min.css'
 
 // window.Vue = Vue;
 
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
+
+Vue.use(SuiVue)
 
 /* eslint-disable no-unused-vars */
 const app = new Vue({
